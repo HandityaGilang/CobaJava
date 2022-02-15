dde80230238d05a7e59effaa1eb86d894f89bc0c
HandityaGilang <johanes.handitya@ti.ukdw.ac.id>
2022-02-15 15:18:46 +0700

    Initial commit

---
+++ src/Lampu.java
@@ -0,0 +1,20 @@
+public class Lampu {
+    private String warna;
+
+    public void nyala(){
+
+    }
+
+    public void mati(){
+
+    }
+
+    public void gantiWarna(String warnaBaru){
+
+    }
+
+    public String warnaSekarang(){
+        return warna;
+    }
+
+}
